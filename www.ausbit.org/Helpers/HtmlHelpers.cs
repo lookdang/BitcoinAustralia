d4e65dea19b3fd87000f645147b6811cb0c89934
Cameron Garnham <da2ce7@gmail.com>
2011-08-01 04:44:04 +1000

    anymore

---
+++ www.ausbit.org/Helpers/HtmlHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+ 
+namespace www.ausbit.org.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static MvcHtmlString NewlineToBr(this HtmlHelper helper, string input)
+        {
+            var result = string.Join(
+                "<br/>",
+                input
+                    .Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                    .Select(x => HttpUtility.HtmlEncode(x))
+            );
+            return MvcHtmlString.Create(result);
+        }
+    }
+}
